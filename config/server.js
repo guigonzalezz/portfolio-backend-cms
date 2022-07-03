35104e2b4f8b9f33a636aa0e8cef04cbae6be446
guigonzalezz <gcgonzalez99@hotmail.com>
2022-07-03 19:54:30 -0300

    feat:init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e031d47819693967b9c86dd41504e94a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b82f4462-e71c-4aaa-a3ef-051d6b98f5fa'
+};
