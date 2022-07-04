22ebb92d7df610aa186e95c4c82f7d37aafd07ca
guigonzalezz <gcgonzalez99@hotmail.com>
2022-07-03 21:46:33 -0300

    chore: changing for strapi v4

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e031d47819693967b9c86dd41504e94a'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'b82f4462-e71c-4aaa-a3ef-051d6b98f5fa'
-};
