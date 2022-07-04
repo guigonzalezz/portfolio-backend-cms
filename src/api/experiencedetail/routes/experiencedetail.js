b9a9f13ab8ca08c7d28853d14f9783f341f288a2
guigonzalezz <gcgonzalez99@hotmail.com>
2022-07-04 09:45:53 -0300

    feat: created all the content models

---
+++ src/api/experiencedetail/routes/experiencedetail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * experiencedetail router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::experiencedetail.experiencedetail');
